f76aabde4396b267559c0b91f24db3e0f97b85f4
Wu <72327178+Usaodon@users.noreply.github.com>
2024-01-23 14:31:20 +0800

    Update sft.sh

--- sft.sh
+++ sft.sh
@@ -1,2 +1,2 @@
 PYPATH=/data/miniconda3/envs/wdm/bin
-CUDA_VISIBLE_DEVICES=7 $PYPATH/python train_qlora.py --train_args_file /data/wdm/wechat/train_args/qlora/baichuan-sft-qlora.json
+CUDA_VISIBLE_DEVICES=7 $PYPATH/python train_lora.py --train_args_file train_args/lora/chatglm2-sft-qlora.json
